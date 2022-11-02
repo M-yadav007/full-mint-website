cabac0fa33c065475d1f24c7ab31ea6c75aa79b1
Mayank yadav <mayank.yad2004@gmail.com>
2022-11-03 03:09:16 +0530

    CONTRACT AND DEPLOYMENT

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+/*const dotenv=require("dotenv");
+dotenv.config();*/
 
+task("accounts","prints the list of accounts",async(taskargs,hre)=>{
+  const accounts =await hre.ethers.getSigners();
+
+  for(const acocunt of accounts)
+  {
+    console.log(account.address);
+  }
+});
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    goerli:
+    {
+      url:"https://goerli.infura.io/v3/c44ea13f341d48219a4856f35c2f0621",
+      accounts: [`e7a77b3f2a687fd4fc0cf4883e9dbbfa27f9c14907650aa409088987e8f04c66`]
+    },
+  },
+  etherscan:
+  {
+    apiKey:process.env.REACT_APP_ETHERSCAN_KEY
+  }
 };
---
+++ scripts/deployRoboPunksNFT.js
@@ -0,0 +1,20 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  
+  const RoboPunksNFT = await hre.ethers.getContractFactory("RoboPunksNFT");
+  const roboPunksNFT = await RoboPunksNFT.deploy();
+
+  await roboPunksNFT.deployed();
+
+  console.log( "RoboPunks NFT is deployed to:",roboPunksNFT.address );
+}
+
+
+main()
+  .then(()=>process.exit(0))
+  .catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
